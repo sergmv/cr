6ff4791ade287913627b13fe014c976cf9996619
SMedintsev <sergmedin@gmail.com>
2015-06-03 13:05:54 +0300

    no message

--- www/js/index.js
+++ www/js/index.js
@@ -33,23 +33,12 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        
         $("#lock").click(function () {
             alert("lock orientation landscape");
             screen.lockOrientation('landscape');
             /*window.shouldRotateToOrientation('landscape');*/
         });
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
     }
 };
 
