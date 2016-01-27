dd4b985d7c0c0bb8867359929f54dfe18bacd6db
SMedintsev <sergmedin@gmail.com>
2016-01-27 23:00:38 +0200

    no message

--- www/app/Controllers/MainCtrl.js
+++ www/app/Controllers/MainCtrl.js
@@ -5,5 +5,26 @@
 
         $scope.test = function() {
             alert("alert test");
+            
+            var scheme;
+
+            // Don't forget to add the cordova-plugin-device plugin for `device.platform`
+            if (device.platform === 'iOS') {
+                scheme = 'twitter://';
+            }
+            else if (device.platform === 'Android') {
+                scheme = 'com.twitter.android';
+            }
+
+            appAvailability.check(
+                scheme,       // URI Scheme or Package Name
+                function () {  // Success callback
+                    alert(scheme + ' is available :)');
+                },
+                function () {  // Error callback
+                    alert(scheme + ' is not available :(');
+                }
+            );
+            
         };
     }]);
