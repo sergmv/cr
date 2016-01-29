2e4d269d150b354701d97c3aad158f4abea10a2f
SMedintsev <sergmedin@gmail.com>
2016-01-30 00:42:01 +0200

    no message

--- www/js/index.js
+++ www/js/index.js
@@ -30,7 +30,7 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
-    //
+    //new branch
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
