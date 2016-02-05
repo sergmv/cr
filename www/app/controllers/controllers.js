2162cfa6896d854684cd83bd27712cffb82204f3
SMedintsev <sergmedin@gmail.com>
2016-02-05 15:49:29 +0200

    no message

--- www/app/controllers/controllers.js
+++ www/app/controllers/controllers.js
@@ -1,6 +1,6 @@
 angular.module('climeride.controllers', [])
     .controller('welcomeCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
-//        alert('welcomeCtrl');
+        //        alert('welcomeCtrl');
         //        $scope.greeting = "dsds";
         //
         //        $scope.test = function() {
@@ -17,77 +17,77 @@ angular.module('climeride.controllers', [])
         };
     }])
     .controller('setupCtrl', ['$scope', '$http', '$location', '$route', function ($scope, $http, $location, $route) {
-//        alert('setupCtrl');
-        
-//                $scope.uberIsInstalled = false;
-//                $scope.lyftIsInstalled = true;
-        
-                $scope.iOSUber = 'uber:';
-                $scope.iOSLyft = 'lyft:';
-                
-                $scope.androidUber = 'com.ubercab';
-                $scope.androidLyft = 'me.lyft.android';
-        
-                $scope.init = function () {
+        //        alert('setupCtrl');
 
-                    if (typeof device == "undefined") {
-                        return;
-                    }
-                    alert(device.platform);
-        
-                    var schemeUber;
-                    var schemeLyft;
-        
-                    // Don't forget to add the cordova-plugin-device plugin for `device.platform`
-                    if (device.platform === 'iOS') {
-                        schemeUber = $scope.iOSUber;
-                        schemeLyft = $scope.iOSLyft;;
-                    }
-                    else if (device.platform === 'Android') {
-                        schemeUber = $scope.androidUber;
-                        schemeLyft = $scope.androidLyft;
-                    }
-        
-                    appAvailability.check(
-                        schemeUber,       // URI Scheme or Package Name
-                        function () {  // Success callback
-                            $scope.uberIsInstalled = true;
-//                            alert("Uber installed");
-                            $scope.$digest();
-                        },
-                        function () {  // Error callback
-                            $scope.uberIsInstalled = false;
-//                            alert("Uber not installed");
-                            $scope.$digest();
-                        }
-                    );
-                    
-                    appAvailability.check(
-                        schemeLyft,       // URI Scheme or Package Name
-                        function () {  // Success callback
-                            $scope.lyftIsInstalled = true;
-//                            alert("Lyft installed");
-                            $scope.$digest();
-                        },
-                        function () {  // Error callback
-                            $scope.lyftIsInstalled = false;
-//                            alert("Lyft not installed");
-                            $scope.$digest();
-                        }
-                    );
-                };
-                
-                $scope.init();
-        
-                $scope.installApp = function (appName) {
-                    
-                };
-        
+        //                $scope.uberIsInstalled = false;
+        //                $scope.lyftIsInstalled = true;
+
+        $scope.iOSUber = 'uber:';
+        $scope.iOSLyft = 'lyft:';
+
+        $scope.androidUber = 'com.ubercab';
+        $scope.androidLyft = 'me.lyft.android';
+
+        $scope.init = function () {
+
+            if (typeof device == "undefined") {
+                return;
+            }
+            alert(device.platform);
+
+            var schemeUber;
+            var schemeLyft;
+
+            // Don't forget to add the cordova-plugin-device plugin for `device.platform`
+            if (device.platform === 'iOS') {
+                schemeUber = $scope.iOSUber;
+                schemeLyft = $scope.iOSLyft;;
+            }
+            else if (device.platform === 'Android') {
+                schemeUber = $scope.androidUber;
+                schemeLyft = $scope.androidLyft;
+            }
+
+            appAvailability.check(
+                schemeUber,       // URI Scheme or Package Name
+                function () {  // Success callback
+                    $scope.uberIsInstalled = true;
+                    //                            alert("Uber installed");
+                    $scope.$digest();
+                },
+                function () {  // Error callback
+                    $scope.uberIsInstalled = false;
+                    //                            alert("Uber not installed");
+                    $scope.$digest();
+                }
+            );
+
+            appAvailability.check(
+                schemeLyft,       // URI Scheme or Package Name
+                function () {  // Success callback
+                    $scope.lyftIsInstalled = true;
+                    //                            alert("Lyft installed");
+                    $scope.$digest();
+                },
+                function () {  // Error callback
+                    $scope.lyftIsInstalled = false;
+                    //                            alert("Lyft not installed");
+                    $scope.$digest();
+                }
+            );
+        };
+
+        $scope.init();
+
+        $scope.installApp = function (appName) {
+            window.open()
+        };
+
         $scope.goToPage = function () {
             $location.path($scope.pageName);
         };
 
-        $scope.getAppStatusClass = function(isInstalled) {
+        $scope.getAppStatusClass = function (isInstalled) {
             if (isInstalled) {
                 return 'app-status-installed';
             } else {
