dbba57e7cd42912651ff874461c1c71c12e39578
SMedintsev <sergmedin@gmail.com>
2016-02-11 18:18:26 +0200

    update styles: set sizes as vw and vh add new icons

--- www/app/controllers/controllers.js
+++ www/app/controllers/controllers.js
@@ -19,8 +19,8 @@ angular.module('climeride.controllers', [])
     .controller('setupCtrl', ['$scope', '$http', '$location', '$route', function ($scope, $http, $location, $route) {
         //alert('setupCtrl');
 
-        //scope.uberIsInstalled = false;
-        //$scope.lyftIsInstalled = true;
+        $scope.uberIsInstalled = false;
+        $scope.lyftIsInstalled = true;
 
 
         $scope.iOSUber = 'uber:';
